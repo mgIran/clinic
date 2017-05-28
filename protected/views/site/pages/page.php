7dd12c22394d576a55388394cd389ee318655626
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-05-28 10:06:52 +0430

    Fix Bugs

--- protected/views/site/pages/page.php
+++ protected/views/site/pages/page.php
@@ -5,18 +5,8 @@
 $this->pageTitle= Yii::app()->name . ' - '.$model->title;
 ?>
 <div class="page">
-    <div class="page-heading">
-        <div class="container">
-            <h1><?php echo $model->title; ?></h1>
-        </div>
-    </div>
     <div class="container page-content">
-        <div class="row">
-            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-                <div class="row">
-                    <div class="text-content"><?= $model->summary; ?></div>
-                </div>
-            </div>
-        </div>
+        <h1><?php echo $model->title; ?></h1>
+        <div class="text-content"><?= $model->summary; ?></div>
     </div>
 </div>
